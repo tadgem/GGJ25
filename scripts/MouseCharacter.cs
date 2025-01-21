cb3c6d5249a9b33496adb90e208105648272a932
tadgem <98434757+tadgem@users.noreply.github.com>
2025-01-21 21:35:31 +0000

    basic movement working

---
+++ scripts/MouseCharacter.cs
@@ -0,0 +1,124 @@
+using Godot;
+using System;
+using System.Reflection.Metadata;
+
+public partial class MouseCharacter : CharacterBody3D
+{
+	[Export]
+	public float MouseSensitivity = 0.33f;
+	[Export]
+	public float Acceleration = 30.0f;
+	[Export]
+	public float MoveSpeed = 30.0f;
+	[Export]
+	public float MovementDeadZone = 0.2f;
+	[Export]
+	public float RotationSpeed = 12.0f;
+
+	private Vector2 _mouseDir = Vector2.Zero;
+	private Vector3 _lastMovementDir = Vector3.Back;
+
+
+	private Node3D _pivot = null;
+	private Node3D _playerModel = null;
+	private Camera3D _cam = null;
+	private AnimationTree _anim = null;
+
+
+	private void HandleAnimationParams()
+	{
+		if(Velocity.Length() > MovementDeadZone)
+		{
+			_anim.Set("parameters/conditions/moving", true);
+			_anim.Set("parameters/conditions/idle", false);
+			_anim.Set("parameters/conditions/jump", false);
+		}
+		else
+		{
+			_anim.Set("parameters/conditions/moving", false);
+			_anim.Set("parameters/conditions/idle", true);
+			_anim.Set("parameters/conditions/jump", false);
+		}
+	}
+
+    public override void _Ready()
+    {
+        base._Ready();
+		_pivot = GetNode<Node3D>("CamPivot");
+		_cam = GetNode<Camera3D>("CamPivot/SpringArm3D/Camera3D");
+		_playerModel = GetNode<Node3D>("Model");
+		_anim = GetNode<AnimationTree>("Model/AnimationTree");
+		_anim.Active = true;
+		GD.Print("Horrible");
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+
+
+		if (@event is InputEventMouseButton mouseButtonEvent && mouseButtonEvent.IsPressed())
+		{
+			if(mouseButtonEvent.ButtonIndex == MouseButton.Left)
+			{
+				Input.MouseMode = Input.MouseModeEnum.Captured;
+			}
+		}
+		
+		if(@event is InputEventKey keyEvent)
+		{
+			if(keyEvent.Keycode == Key.Escape && keyEvent.IsPressed())
+			{
+				Input.MouseMode = Input.MouseModeEnum.Visible;
+			}
+		}
+
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event is InputEventMouseMotion mouseMotionEvent && Input.MouseMode == Input.MouseModeEnum.Captured)
+		{
+			_mouseDir = mouseMotionEvent.ScreenRelative * MouseSensitivity;	
+		}
+		
+	}
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+		Vector3 euler = _pivot.Rotation;
+		euler.X += _mouseDir.Y * (float) delta;
+		euler.Y -= _mouseDir.X * (float) delta;
+
+		euler.X = Mathf.Clamp(euler.X, (float) -Math.PI / 6.0f, (float)Math.PI / 3.0f);
+		_pivot.Rotation = euler;
+
+		_mouseDir = Vector2.Zero;
+
+		var raw_input = Input.GetVector("move_left", "move_right", "move_backward", "move_forward");
+		Vector3 forward = -_cam.GlobalBasis.Z;
+		Vector3 right = _cam.GlobalBasis.X;
+
+		Vector3 move_dir = (forward * raw_input.Y) + (right * raw_input.X);
+		move_dir.Y = 0.0f;
+		move_dir = move_dir.Normalized();
+
+		Velocity = Velocity.MoveToward(move_dir * MoveSpeed, Acceleration * (float) delta);
+		MoveAndSlide();
+
+		HandleAnimationParams();
+
+		if(move_dir.Length() > MovementDeadZone)
+		{
+			_lastMovementDir = move_dir;
+			float target_angle = Vector3.Back.SignedAngleTo(_lastMovementDir, Vector3.Up);
+			Vector3 playerEuler = _playerModel.GlobalRotation;
+			playerEuler.Y = Mathf.LerpAngle(_playerModel.Rotation.Y, target_angle, RotationSpeed * (float) delta);
+			playerEuler.X = 0.0f;
+
+			_playerModel.GlobalRotation = playerEuler;
+		}
+
+    }
+}
