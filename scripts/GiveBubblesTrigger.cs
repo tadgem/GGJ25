e409e74e12f96cbd19bbb78bff1c479c477dd2f5
Tadge <98434757+tadgem@users.noreply.github.com>
2025-01-26 11:51:01 +0000

    update give bubbles to have a sound

--- scripts/GiveBubblesTrigger.cs
+++ scripts/GiveBubblesTrigger.cs
@@ -1,12 +1,20 @@
 using Godot;
 using System;
+using System.Net.Http.Headers;
 
 public partial class GiveBubblesTrigger : Area3D
 {
+
+	[Export]
+	public AudioStream OnGiveBubblesAudio;
+
+	private AudioStreamPlayer _player;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		BodyEntered += OnBodyEntered;
+		_player = new AudioStreamPlayer();
+		AddChild(_player);
 	}
 
     private void OnBodyEntered(Node3D body)
@@ -14,7 +22,15 @@ public partial class GiveBubblesTrigger : Area3D
 		if(body is MouseCharacter mouse)
 		{
 			mouse.GiveBubbles();
-			QueueFree();
+			_player.Stream = OnGiveBubblesAudio;
+			_player.Play();
+			_player.Finished += OnAudioFinished;
 		}
     }
+
+    private void OnAudioFinished()
+    {
+		QueueFree();
+    }
+
 }
